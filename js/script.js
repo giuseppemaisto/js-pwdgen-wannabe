b55ab929facec73c66c863965233ddab5d380167
giuseppemaisto <gmaisto98@gmail.com>
2022-11-24 14:25:58 +0100

    dichiarate variabili

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*STRUTTURA
+1 - Dare la possibilità di inserire il nome
+2 - Dare la possibilità di inserire il cognome
+3 - Dare la possibilità di inserire il colore preferito
+4 - Concatenare i valori delle variabili concatenando anche il numero 21 a fine stringa 
+5 - Visualizzare a schermo in HTML*/
+
+
+//1-dare la possibilità di inserire il nome
+let nome_utente = prompt('inserisci il tuo nome')
+
+//2-dare la possibilità di inserire il cognome
+let cognome_utente = prompt('inserisci il tuo cognome')
+
+//3 - Dare la possibilità di inserire il colore preferito
+let colore_preferito = prompt('inserisci il tuo colore ')
+
